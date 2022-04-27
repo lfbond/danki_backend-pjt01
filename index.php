5646c7df57e8d04da9e914c024de159acb1f2cdb
Luis Felipe de Souza Bond <69223872+lfbond@users.noreply.github.com>
2022-04-26 21:15:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <!-- meta tag de responsivo e acentuação -->
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <!-- meta tag de otimização SEO-->
+            <meta name="description" content=""> 
+            <meta name="keywords" content="">
+            <meta name="author" content="F1 Technology">
+        <!-- tag de titulo do site -->
+            <title>Projeto 01</title>
+            <link rel="shotcut icon" type="image-x/png" href="">
+        <!-- tag de icon do awesome -->
+            <!-- <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+            <link rel="stylesheet" href="css/fontawesome.css" type="text/css"> -->
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+        <!-- tag de font do google font -->
+        <!-- tags de css -->
+            <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+
+        <header>
+            <div class="center">
+                <div class="logo">Logomarca</div>
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Serviço</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </nav><!-- fim nav desktop -->
+
+                <nav class="mobile">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Serviço</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </nav><!-- fim nav mobile -->
+            </div><!-- fim center -->
+        </header><!-- fim header -->
+
+        <section class="banner-principal">
+            <div class="center">
+                <form action="">
+                    <h2>Qual o seu Melhor e-mail?</h2>
+                    <input type="email" name="email" required>
+                    <input type="submit" name="acao" value="Cadastrar!">
+                </form>
+            </div><!-- fim center -->
+        </section><!-- fim section banner-principal -->
+
+        <section class="descricao-autor">
+            <div class="center">
+                <div class="w50">
+                    <h2>Luís Felipe Bond</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis sed eros ut porttitor. Maecenas nulla mi, vulputate vitae nisl vitae, volutpat imperdiet leo. Donec a tristique diam. Proin quis turpis dui. Curabitur et odio facilisis magna commodo pellentesque sit amet molestie neque. Duis id dolor eu nibh viverra elementum. Donec sagittis, diam et vulputate auctor, leo nulla lacinia ipsum, nec ullamcorper nisi odio nec elit. Vivamus massa arcu, fermentum sit amet imperdiet vitae, tristique vitae mauris. Aenean quis elit convallis magna bibendum ornare. </p>
+                    <p>In hac habitasse platea dictumst. Integer gravida, tortor ac egestas venenatis, justo purus gravida neque, a eleifend tellus diam eget risus. Nunc sapien erat, condimentum eget laoreet tristique, suscipit id elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam nec nunc turpis. Phasellus nec lobortis libero, ut fringilla enim. Aliquam at lectus tincidunt, dictum neque sed, molestie dolor. Vestibulum ac mattis ipsum. Vivamus bibendum bibendum felis, sit amet accumsan est sollicitudin eu. Nunc accumsan purus dictum mauris vehicula efficitur. Integer ultricies nunc sit amet nibh pharetra, vel iaculis dolor hendrerit. </p>
+                </div><!-- fim w50 -->
+
+                <div class="w50">
+                    <img src="./images/foto.jpg" alt="Minha foto">
+                </div><!-- fim w50 -->
+            </div><!-- fim center -->
+        </section><!-- fim section descricao-autor -->
+
+        <section class="especialidades">
+            
+            <div class="center">
+                <h2 class="title">Especialidades</h2>
+
+                <div class="box-especialidade">
+                    <h3>Icone1</h3>
+                    <h3>CSS3</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis sed eros ut porttitor. Maecenas nulla mi, vulputate vitae nisl vitae, volutpat imperdiet leo.</p>
+                </div><!-- fim box-especialidade -->
+
+                <div class="box-especialidade">
+                    <h3>Icone2</h3>
+                    <h3>HTML5</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis sed eros ut porttitor. Maecenas nulla mi, vulputate vitae nisl vitae, volutpat imperdiet leo.</p>
+                </div><!-- fim box-especialidade -->
+
+                <div class="box-especialidade">
+                    <h3>Icone3</h3>
+                    <h3>Javascript</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis sed eros ut porttitor. Maecenas nulla mi, vulputate vitae nisl vitae, volutpat imperdiet leo.</p>
+                </div><!-- fim box-especialidade -->
+            </div><!-- fim center -->
+        </section><!-- fim section especialidades -->
+
+        <section class="depoimentos-servicos">
+            <div class="center">
+                <div class="w50">
+                    <h2 class="title">Depoimentos</h2>
+                    <div class="depoimentos-single">
+                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis sed eros ut porttitor. Maecenas nulla mi, vulputate vitae nisl vitae, volutpat imperdiet leo.</p>
+                        <p class="nome-autor">Lorem Ipsum</p>
+                    </div><!-- fim depoimentos-single -->
+
+                    <div class="depoimentos-single">
+                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis sed eros ut porttitor. Maecenas nulla mi, vulputate vitae nisl vitae, volutpat imperdiet leo.</p>
+                        <p class="nome-autor">Lorem Ipsum</p>
+                    </div><!-- fim depoimentos-single -->
+
+                    <div class="depoimentos-single">
+                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis sed eros ut porttitor. Maecenas nulla mi, vulputate vitae nisl vitae, volutpat imperdiet leo.</p>
+                        <p class="nome-autor">Lorem Ipsum</p>
+                    </div><!-- fim depoimentos-single -->
+                </div><!-- fim w50 -->
+                
+                <div class="w50">
+                    <h2 class="title">Serviços</h2>
+                    <div class="servicos">
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        </ul>
+                    </div><!-- fim servicos -->
+                </div><!-- fim w50 -->
+            </div><!-- fim center -->
+        </section><!-- fim section depoimentos-servicos -->
+
+        <footer>
+            <div class="center">
+                <p>Todos os direitos reservados!</p>
+            </div>
+        </footer>
+    </body>
+</html>
